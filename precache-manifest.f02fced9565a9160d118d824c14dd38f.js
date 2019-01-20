8b02d4d037d4c962243631cfc27c2a0931a32080
lindolo25 <lindolo25@outlook.com>
2019-01-20 17:17:49 -0500

    Updates

---
+++ precache-manifest.f02fced9565a9160d118d824c14dd38f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "d4b0f42f554634aa93ca",
+    "url": "/clicky-game/static/js/main.d4b0f42f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d4b0f42f554634aa93ca",
+    "url": "/clicky-game/static/css/main.4e27db4a.chunk.css"
+  },
+  {
+    "revision": "aaa1c6eb2f7c2b69d2387d0b6efdc9e4",
+    "url": "/clicky-game/index.html"
+  }
+];
