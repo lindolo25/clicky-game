2a4d3032b5813891b1116b197bdee503965033d3
Rolando Perez <lindolo25@outlook.com>
2019-03-28 14:01:01 -0400

    added a docker container to this project.

--- src/components/score.js
+++ src/components/score.js
@@ -5,7 +5,7 @@ var Score = (props) =>
 {
     return (
         <header className="row">
-            <div className="col-12 col-sm-4 text-center">Example Title</div>
+            <div className="col-12 col-sm-4 text-center">Clicky-Game</div>
             <div className="col-12 col-sm-4 text-center">{props.message}</div>
             <div className="col-12 col-sm-4 text-center">Example {props.currentScore} / {props.topScore}</div>
         </header>
